7f49c1aab42105fad3bb765f28e15381432eec03
Douglas Nicihoka <31430383+doug-nicihoka@users.noreply.github.com>
2017-10-22 20:45:18 -0200

    Correção de Grafico.h
    Declaração da classe estava errada (estava tentando se declarar a classe Eixo na linha 8, ao invés da classe Grafico).

--- EP2/Grafico.h
+++ EP2/Grafico.h
@@ -5,23 +5,22 @@
 #include "Serie.h"
 #include "Tela.h"
 
-class Eixo {
-    protected:
+
+class Grafico {
+    private:
         Eixo* x;
         Eixo* y;
         Serie* serie;
     public:
-        /**
-        * Cria um grafico informando os eixos e a serie.
-        */
-        Grafico(Eixo* x, Eixo* y, Serie* Serie);
+        Grafico(Eixo* x, Eixo* y, Serie* serie);
         virtual ~Grafico();
 
         Eixo* getEixoX();
         Eixo* getEixoY();
         Serie* getSerie();
+
         /**
-        * Desenha o grafico na tela.
+        * Desenha o Grafico na Tela.
         */
         void desenhar();
 };
