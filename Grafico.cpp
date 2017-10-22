e8fd588f4d6b11e95991b2bf843442c4adae832f
Douglas Nicihoka <31430383+doug-nicihoka@users.noreply.github.com>
2017-10-22 20:55:01 -0200

    Remoção de std:: e mudança na indentação
    o prefixo std:: só é necessário quando se usam funções das bibliotecas padrão, como "cout", "endl", "string", "abs", etc. Indentação do for levemente alterada.

--- Grafico.cpp
+++ Grafico.cpp
@@ -19,11 +19,12 @@ Eixo* Grafico::getSerie() {
 
 void Grafico::desenhar() {
   Tela* t = new Tela;
-  t->setEixoX(std:x->getTitulo(), x->getMinimo(), x->getMaximo());
-  t->setEixoY(std:y->getTitulo(), y->getMinimo(), y->getMaximo());
-  for(int i = 0; i < serie->getQuantidade(); i++) {
+  t->setEixoX(x->getTitulo(), x->getMinimo(), x->getMaximo());
+  t->setEixoY(y->getTitulo(), y->getMinimo(), y->getMaximo());
+  
+  for(int i = 0; i < serie->getQuantidade(); i++)
     t->plotar(serie->getNome(), serie->getPosicao(i)->getX(), serie->getPosicao(i)->getY());
-  }
+      
   t->mostrar();
   delete t;
 }
