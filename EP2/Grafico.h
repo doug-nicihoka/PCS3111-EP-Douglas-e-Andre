7038815c760c1448b97fb9bc191de3d7207ac814
andrebarbs13 <31722863+andrebarbs13@users.noreply.github.com>
2017-10-22 19:42:44 -0200

    Update Grafico.h

--- EP2/Grafico.h
+++ EP2/Grafico.h
@@ -1,1 +1,29 @@
+#ifndef GRAFICO_H
+#define GRAFICO_H
 
+#include <string>
+#include "Eixo.h"
+#include "Serie.h"
+#include "Tela.h"
+
+class Eixo {
+    protected:
+        Eixo* x;
+        Eixo* y;
+        Serie* Serie;
+    public:
+        /**
+        * Cria um grafico informando os eixos e a serie.
+        */
+        Grafico(Eixo* x, Eixo* y, Serie* Serie);
+        virtual ~Grafico();
+
+        Eixo* getEixoX();
+        Eixo* getEixoY();
+        Serie* getSerie();
+        /**
+        * Desenha o grafico na tela.
+        */
+};
+
+#endif // EIXO_H
