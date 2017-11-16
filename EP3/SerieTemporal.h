321316a07145c83a598114b6578fec037a0ef527
Douglas Nicihoka <31430383+doug-nicihoka@users.noreply.github.com>
2017-11-16 05:03:33 -0200

    Adição da classe SerieTemporal
    Método adicionar(double x, double y) removido, método adicionar(double valor) alterado (não é mais necessário checar antes de adicionar os pontos).

---
+++ EP3/SerieTemporal.h
@@ -0,0 +1,35 @@
+#ifndef SERIETEMPORAL_H
+#define SERIETEMPORAL_H
+
+#include "Serie.h"
+
+class SerieTemporal : public Serie {
+    protected:
+        double tempo = 1;
+    public:
+        /**
+        * Cria uma SerieTemporal informando o nome da Serie e o nome do
+        * canalY. O nome do canal X deve ser obrigatoriamente "Tempo".
+        */
+        SerieTemporal(std::string nome, std::string nomeDoCanalY);
+        virtual ~SerieTemporal();
+
+        /**
+        * Adiciona um novo Ponto a Serie, no instante seguinte ao do
+        * ponto anterior adicionado por este método. O primeiro ponto
+        * deve ser adicionado no instante 1.
+        */
+        virtual void adicionar(double valor);
+
+        /**
+        * Adiciona um Ponto a Serie informando a coordenada x e y.
+        * Caso já exista um ponto cuja coordenada x seja suficientemente
+        * próxima ao do valor x informado, ao invés e adicionar o ponto,
+        * altere o valor anterior (mantendo sua posição no arranjo)
+        * ao considerar a nova coordenada y.
+        * Caso a coordenada x do ponto for < 1, o ponto não deve ser adicionado.
+        */
+        virtual void adicionar(double x, double y);
+};
+
+#endif // SERIETEMPORAL_H
