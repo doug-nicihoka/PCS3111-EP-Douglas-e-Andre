a6999e765897d8d0a858f4d13fb4132decf5b562
Douglas Nicihoka <31430383+doug-nicihoka@users.noreply.github.com>
2017-11-19 15:39:20 -0200

    Alteração de Serie.cpp
    Arquivo desatualizado (do EP2) tinha sido adicionado por engano.

--- EP3/SerieTemporal.cpp
+++ EP3/SerieTemporal.cpp
@@ -1,30 +1,11 @@
 #include "SerieTemporal.h"
-#include <cmath>
 
-SerieTemporal::SerieTemporal(std::string nome, std::string nomeDoCanalY) : Serie (nome, "Tempo", nomeDoCanalY) {}
+SerieTemporal::SerieTemporal(std::string nome, std::string nomeDoCanalY) :
+    Serie (nome, "Tempo", nomeDoCanalY) {}
 
 SerieTemporal::~SerieTemporal() {}
 
 void SerieTemporal::adicionar(double valor) {
-    for (int i = 0; i < quantidade; i++)
-        if (std::abs((getPosicao(i)->getX()) - tempo) <= 1e-5) {
-            eixoY[i] = valor;
-            tempo += 1;
-            return;
-        }
-
-    Serie::adicionar(tempo, valor);
-    tempo += 1;
-}
-
-void SerieTemporal::adicionar(double x, double y) {
-    if (x < 1)
-        return;
-    for (int i = 0; i < quantidade; i++)
-        if (std::abs(getPosicao(i)->getX() - x) <= 1e-5) {
-            eixoY[i] = y;
-            return;
-        }
-
-    Serie::adicionar(x, y);
+    pontos->push_back(new Ponto(tempo, valor));
+    tempo++;
 }
