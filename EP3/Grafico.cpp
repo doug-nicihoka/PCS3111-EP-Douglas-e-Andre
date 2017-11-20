238ff9393cf7b2acf8fd34162a82c71144e55080
Douglas Nicihoka <31430383+doug-nicihoka@users.noreply.github.com>
2017-11-19 22:27:50 -0200

    Adição de Grafico.cpp
    Construtor da classe agora lança exceções caso parâmetros não sejam válidos. Método desenhar() alterado para que todos os Pontos da list de Series sejam plotados na Tela.

---
+++ EP3/Grafico.cpp
@@ -0,0 +1,47 @@
+#include "Grafico.h"
+#include <stdexcept>
+
+Grafico::Grafico(Eixo* x, Eixo* y, std::vector<Serie*>* series) : x (x), y (y), series (series) {
+    if (!x->temOrientacaoHorizontal())
+        throw new std::logic_error ("Eixo x com orientacao vertical.");
+    else if (y->temOrientacaoHorizontal())
+        throw new std::logic_error ("Eixo y com orientacao horizontal.");
+    else if (!series)
+        throw new std::logic_error ("Nao ha Serie.");
+}
+
+Grafico::~Grafico() {
+    delete x;
+    delete y;
+    delete series;
+}
+
+Eixo* Grafico::getEixoX() {
+    return x;
+}
+
+Eixo* Grafico::getEixoY() {
+    return y;
+}
+
+std::vector<Serie*>* Grafico::getSeries() {
+    return series;
+}
+
+void Grafico::desenhar() {
+    Tela* t = new Tela;
+    t->setEixoX(x->getTitulo(), x->getMinimo(), x->getMaximo());
+    t->setEixoY(y->getTitulo(), y->getMinimo(), y->getMaximo());
+
+    // Iterator da Serie
+    std::vector<Serie*>::iterator sIt;
+    // Iterator do Ponto
+    std::vector<Ponto*>::iterator pIt;
+    for (sIt = series->begin(); sIt != series->end(); ++sIt)
+        for(pIt = (*sIt)->getPontos()->begin(); pIt != (*sIt)->getPontos()->end(); ++pIt)
+            t->plotar((*sIt)->getNome(), (*pIt)->getX(), (*pIt)->getY());
+
+
+    t->mostrar();
+    delete t;
+}
