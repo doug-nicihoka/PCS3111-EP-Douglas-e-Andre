4c84178aea66f9c18e9aaf255776580858677055
Douglas Nicihoka <31430383+doug-nicihoka@users.noreply.github.com>
2017-11-21 17:52:36 -0200

    Mudança na mensagem de erro de EixoDinamico

--- EP3/EixoDinamico.cpp
+++ EP3/EixoDinamico.cpp
@@ -3,20 +3,20 @@
 
 EixoDinamico::EixoDinamico(std::string titulo, double minimoPadrao, double maximoPadrao, std::list<Serie*>* series, bool orientacaoHorizontal) :
     Eixo (titulo, minimoPadrao, maximoPadrao, orientacaoHorizontal) {
-    if (series) {                                                               //Se o ponteiro series n„o È nulo
-        if (!series->empty()) {                                                 //Se a lista para qual o ponteiro aponta n„o est· vazia
+    if (series) {                                                               //Se o ponteiro series n√£o √© nulo
+        if (!series->empty()) {                                                 //Se a lista para qual o ponteiro aponta n√£o est√° vazia
             std::list<Serie*>::iterator it;
             double min, max;
             int aux = 0;
             for (it = series->begin(); it != series->end(); ++it) {             // Varre a lista inteira
                 if (!(*it)->estaVazia()) {                                      // Se sair do loop sem entrar nenhuma vez aqui, lista possui Series vazias
                     if (orientacaoHorizontal) {                                 // Eixo horizontal
-                        if (aux == 0) {                                         // Detecta a primeira vez em que se entra na condiÁ„o da linha 12
+                        if (aux == 0) {                                         // Detecta a primeira vez em que se entra na condi√ß√£o da linha 12
                             max = (*it)->getLimiteSuperior()->getX();
                             min = (*it)->getLimiteInferior()->getX();
                             ++aux;
                         }
-                        else {                                                  // ComeÁa a comparar com valores guardados nas vari·veis min e max
+                        else {                                                  // Come√ßa a comparar com valores guardados nas vari√°veis min e max
                             if (max < (*it)->getLimiteSuperior()->getX())
                                 max = (*it)->getLimiteSuperior()->getX();
 
@@ -24,7 +24,7 @@ EixoDinamico::EixoDinamico(std::string titulo, double minimoPadrao, double maxim
                                 min = (*it)->getLimiteInferior()->getX();
                         }
                     }
-                    else {                                                      // Eixo vertical (an·logo ao eixo horizontal)
+                    else {                                                      // Eixo vertical (an√°logo ao eixo horizontal)
                         if (aux == 0) {
                             max = (*it)->getLimiteSuperior()->getY();
                             min = (*it)->getLimiteInferior()->getY();
@@ -40,7 +40,7 @@ EixoDinamico::EixoDinamico(std::string titulo, double minimoPadrao, double maxim
                     }
                 }
             }
-            if (aux != 0) {                                                     // SÛ entra aqui caso a lista possua pelo menos uma Serie n„o vazia.
+            if (aux != 0) {                                                     // S√≥ entra aqui caso a lista possua pelo menos uma Serie n√£o vazia.
                 if (max - min > 1e-5) {
                     maximo = max;
                     minimo = min;
@@ -48,7 +48,7 @@ EixoDinamico::EixoDinamico(std::string titulo, double minimoPadrao, double maxim
             }
         }
     }
-    else throw new std::logic_error ("Nao ha lista.");
+    else throw new std::logic_error ("ERRO: list de Series nula.");
 }
 
 EixoDinamico::~EixoDinamico() {}
