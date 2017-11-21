d5332ff0580dbbfb118ab532acb95bb29c6bb84f
Douglas Nicihoka <31430383+doug-nicihoka@users.noreply.github.com>
2017-11-21 17:32:59 -0200

    Revisão e melhorias de main.cpp
    , Melhorada a maneira em que os pontos são adicionados (agora código adiciona Series à list conforme o usuário as adiciona). Com isso, variáveis agora desnecessárias foram removidas, e o código simplificado.  Capturas de exceções adicionadas.. Objetos alocados dinamicamente deletados. Carregamento e salvamento de Series implementado.

--- EP3/main.cpp
+++ EP3/main.cpp
@@ -16,21 +16,20 @@
 #include "Tela.h"
 #include "Grafico.h"
 #include "ErroDeArquivo.h"
+#include "PersistenciaDeSerie.h"
 
 using namespace std;
 
 #define COMM "\\\\.\\COM3"
 
 int main() {
-    /* Inicializa e captura exceção em caso de falha */
+    /* Cria e inicializa instancia de InterfaceSerial */
     InterfaceSerial* is = new InterfaceSerial(COMM);
     try {
         is->inicializar();
     }
-    catch (runtime_error *e) {
-        cout << e->what() << endl;
-        delete e;
-        return 0;
+    catch (runtime_error* e) {
+        cout << e->what() << endl; delete e; return 0;
     }
 
 
@@ -43,34 +42,39 @@ int main() {
 
 
     /* Obtém o nome da Serie e os canais escolhidos */
-    vector<string> nomesSeries, canaisX, canaisY;
-    string nome;
-    vector<int> numX, numY;
-    int numero;
-    char continuar = 's';
+	list<Serie*>* series = new list<Serie*>();
+    char continuar;
+
     // Loop válido enquanto o usuário desejar adicionar outra serie
     do {
+        string nome;
         cout << "Informe o nome da serie: ";
         cin >> nome;
-        nomesSeries.push_back(nome);
 
         cout << "Escolha o canal X:" << endl;
         cout << "0) Tempo" << endl;
         for(int i = 1; i <= is->getQuantidadeDeCanais(); i++)
             cout << i << ") " << *(is->getNomeDosCanais() + (i - 1)) << endl;
-        cin >> numero;
-        numX.push_back(numero);
-        if (numero > 0)
-            canaisX.push_back(*(is->getNomeDosCanais() + (numero - 1)));
-        else if (numero == 0)
-            canaisX.push_back("Tempo");
+        int numX;
+        cin >> numX;
+        string canalX;
+        if (numX > 0)
+            canalX = *(is->getNomeDosCanais() + (numX - 1));
+        else if (numX == 0)
+			canalX = "Tempo";
 
         cout << "Escolha o canal Y:" << endl;
         for(int i = 1; i <= is->getQuantidadeDeCanais(); i++)
             cout << i << ") " << *(is->getNomeDosCanais() + (i - 1)) << endl;
-        cin >> numero;
-        numY.push_back(numero);
-        canaisY.push_back(*(is->getNomeDosCanais() + (numero - 1)));
+        int numY;
+        cin >> numY;
+        string canalY;
+		canalY = *(is->getNomeDosCanais() + (numY - 1));
+
+		if (numX > 0)
+			series->push_back(new SerieNormal(nome, canalX, canalY));
+		else if (numX == 0)
+			series->push_back(new SerieTemporal(nome, canalY));
 
         cout << "Adicionar uma outra serie (s/n): ";
         cin >> continuar;
@@ -79,136 +83,140 @@ int main() {
 
 
 
-    /* Obtem o numero de Pontos a adicionar */
-    int quantidade;
-    cout << "Obter quantos pontos? ";
-    cin >> quantidade;
-
-
-
-    /* Obtem os Pontos */
-    cout << "Obtendo os pontos" << endl;
-    list<Serie*>* series = new list<Serie*>();
-    list<Serie*>::iterator itSerie;
-    int contador = 0;
-
+    /* Adiciona os pontos às Series */
     try {
-        // Loop com iterações = num de Series adicionadas
-        for (vector<int>::iterator itX = numX.begin(); itX != numX.end(); ++itX) {
-            if ((*itX) == 0) {  // Se Serie é uma SerieTemporal
-                series->push_back(new SerieTemporal(nomesSeries[contador], canaisY[contador]));
+        // Obtem o numero de Pontos a adicionar
+        int quantidade;
+        cout << "Obter quantos pontos? ";
+        cin >> quantidade;
 
-                if (contador == 0) itSerie = series->begin();
-                else ++itSerie;
+		cout << "Obtendo os pontos" << endl;
 
-                SerieTemporal* st = dynamic_cast<SerieTemporal*>(*itSerie);
-
-                if (st) {
-                    for (int i = 0; i < quantidade; ++i) {
-                            is->atualizar();
-                            st->adicionar(is->getValor(canaisY[contador]));
-                    }
-                }
-            }
-            else {              // Se Serie é uma SerieNormal
-                series->push_back(new SerieNormal(nomesSeries[contador], canaisX[contador], canaisY[contador]));
-
-                if (contador == 0) itSerie = series->begin();
-                else ++itSerie;
-
-                SerieNormal* sn = dynamic_cast<SerieNormal*>(*itSerie);
-
-                if (sn) {
+        for (list<Serie*>::iterator it = series->begin(); it != series->end(); ++it) {
+            if ((*it)->getNomeDoCanalX() == "Tempo" ) {
+                if (SerieTemporal* st = dynamic_cast<SerieTemporal*>(*it)) {
                     for (int i = 0; i < quantidade; ++i) {
                         is->atualizar();
-                        sn->adicionar(is->getValor(canaisX[contador]), is->getValor(canaisY[contador]));
+                        st->adicionar(is->getValor(st->getNomeDoCanalY()));
                     }
                 }
             }
-            ++contador;
-        }
+            else {
+                if (SerieNormal* sn = dynamic_cast<SerieNormal*>(*it)) {
+                    for (int i = 0; i < quantidade; ++i) {
+                        is->atualizar();
+                        sn->adicionar(is->getValor(sn->getNomeDoCanalX()), is->getValor(sn->getNomeDoCanalY()));
+					}
+                }
+            }
+		}
     }
     // Captura exceções dos métodos atualizar e getValor da InterfaceSerial
-    catch (logic_error *e) {
-        cout << e->what() << endl;
-        delete e;
-        return 0;
+    catch (logic_error* e) {
+        cout << e->what() << endl; delete e; return 0;
     }
-    catch (runtime_error *e) {
-        cout << e->what() << endl;
-        delete e;
-        return 0;
+    catch (runtime_error* e) {
+        cout << e->what() << endl; delete e; return 0;
     }
 
 
 
     /* Carrega arquivo com Series */
-    char carregarSerie = 'a';
+    PersistenciaDeSerie* ps;
+    vector<string> seriesPersistidas;
     cout << "Deseja carregar alguma serie? (s/n) ";
-    cin >> carregarSerie;
-    if (carregarSerie == 's') {
-        ifstream entrada;
-        entrada.open(dados, ios::ate);
-        if (entrada.tellg() == 0)
+    cin >> continuar;
+
+    try {
+        ps = new PersistenciaDeSerie(dados);
+    }
+    catch (ErroDeArquivo* e) {
+        cout << e->what() << endl; delete e; return 0;
+    }
+
+    if (continuar == 's') {
+        seriesPersistidas = *(ps->getNomes());
+        if (seriesPersistidas.empty())
             cout << "Sem series salvas." << endl;
         else {
+            do {
+                int i = 0;
+                for (vector<string>::iterator it = seriesPersistidas.begin(); it != seriesPersistidas.end(); ++it)
+                    cout << ++i << ") " << *it << endl;
+                cout << "Escolha a serie para carregar: ";
+                cin >> i;
 
+                series->push_back(ps->obter(seriesPersistidas[i-1]));
+
+                cout << "Deseja carregar outra serie? (s/n) ";
+                cin >> continuar;
+            }
+            while (continuar == 's');
         }
     }
 
 
 
-    /* Define o eixo X */
-    string titulo;
-    double maximo, minimo;
-    char tipo;
+    /* Obtém os eixos */
     Eixo* eixoX;
-    cout << "O eixo X e' estatico ou dinamico (e/d): ";
-    cin >> tipo;
-
-    if(tipo == 'e') {
-        cout << "Informe o titulo: ";
-        cin >> titulo;
-        cout << "Valor minimo: ";
-        cin >> minimo;
-        cout << "Valor maximo: ";
-        cin >> maximo;
-        eixoX = new EixoEstatico(titulo, minimo, maximo, true);
-    }
-    else if (tipo == 'd') {
-        cout << "Informe o titulo: ";
-        cin >> titulo;
-        cout << "Valor minimo padrao: ";
-        cin >> minimo;
-        cout << "Valor maximo padrao: ";
-        cin >> maximo;
-        eixoX = new EixoDinamico(titulo, minimo, maximo, series, true);
-    }
-
-
-
-    /* Define o eixo Y */
     Eixo* eixoY;
-    cout << "O eixo Y e' estatico ou dinamico (e/d): ";
-    cin >> tipo;
 
-    if(tipo == 'e') {
-        cout << "Informe o titulo: ";
-        cin >> titulo;
-        cout << "Valor minimo: ";
-        cin >> minimo;
-        cout << "Valor maximo: ";
-        cin >> maximo;
-        eixoY = new EixoEstatico(titulo, minimo, maximo, false);
+    try {
+        string titulo;
+        double maximo, minimo;
+        char tipo;
+
+        // Define o eixo X
+        cout << "O eixo X e' estatico ou dinamico (e/d): ";
+        cin >> tipo;
+
+        if(tipo == 'e') {
+            cout << "Informe o titulo: ";
+            cin >> titulo;
+            cout << "Valor minimo: ";
+            cin >> minimo;
+            cout << "Valor maximo: ";
+            cin >> maximo;
+            eixoX = new EixoEstatico(titulo, minimo, maximo, true);
+        }
+        else if (tipo == 'd') {
+            cout << "Informe o titulo: ";
+            cin >> titulo;
+            cout << "Valor minimo padrao: ";
+            cin >> minimo;
+            cout << "Valor maximo padrao: ";
+            cin >> maximo;
+            eixoX = new EixoDinamico(titulo, minimo, maximo, series, true);
+        }
+        // Define o eixo Y */
+        cout << "O eixo Y e' estatico ou dinamico (e/d): ";
+        cin >> tipo;
+
+        if(tipo == 'e') {
+            cout << "Informe o titulo: ";
+            cin >> titulo;
+            cout << "Valor minimo: ";
+            cin >> minimo;
+            cout << "Valor maximo: ";
+            cin >> maximo;
+            eixoY = new EixoEstatico(titulo, minimo, maximo, false);
+        }
+        else if (tipo == 'd') {
+            cout << "Informe o titulo: ";
+            cin >> titulo;
+            cout << "Valor minimo padrao: ";
+            cin >> minimo;
+            cout << "Valor maximo padrao: ";
+            cin >> maximo;
+            eixoY = new EixoDinamico(titulo, minimo, maximo, series, false);
+        }
     }
-    else if (tipo == 'd') {
-        cout << "Informe o titulo: ";
-        cin >> titulo;
-        cout << "Valor minimo padrao: ";
-        cin >> minimo;
-        cout << "Valor maximo padrao: ";
-        cin >> maximo;
-        eixoY = new EixoDinamico(titulo, minimo, maximo, series, false);
+    // Captura exceções durante a criação dos eixos
+    catch (runtime_error* e) {
+        cout << e->what() << endl; delete e; return 0;
+    }
+    catch (logic_error* e) {
+        cout << e->what() << endl; delete e; return 0;
     }
 
 
@@ -218,18 +226,68 @@ int main() {
     vectorSeries->reserve(series->size());
     copy(begin(*series), std::end(*series), back_inserter(*vectorSeries));
 
-    Grafico* g;
-    g = new Grafico(eixoX, eixoY, vectorSeries);
-    g->desenhar();
-
+    try {
+        Grafico* g;
+        g = new Grafico(eixoX, eixoY, vectorSeries);
+        g->desenhar();
+        delete g;
+    }
+    // Captura exceções durante a criação do grafico
+    catch (logic_error* e) {
+        cout << e->what() << endl; delete e; return 0;
+    }
 
 
     /* Salva Series */
     cout << "Deseja salvar alguma serie? (s/n) ";
     cin >> continuar;
     if (continuar == 's') {
+        do {
+            int i = 0;
+            for (vector<Serie*>::iterator it = vectorSeries->begin(); it != vectorSeries->end(); ++it)
+                cout << ++i << ") " << (*it)->getNome() << endl;
+            cout << "Escolha a serie para salvar: ";
+            cin >> i;
 
+            string nome;
+            cout << "Salvar a serie com qual nome: ";
+            cin >> nome;
+
+            try {
+                ps->inserir(nome, vectorSeries->at(i-1));
+            }
+            // Captura exceção do método inserir
+            catch (ErroDeArquivo* e) {
+                cout << e->what() << endl; delete e; return 0;
+            }
+
+            cout << "Deseja salvar outra serie? (s/n) ";
+            cin >> continuar;
+        }
+        while (continuar == 's');
     }
+
+    // Remove instancia de PersistenciaDeSerie
+    delete ps;
+
+    // Remove os eixos criados
+    delete eixoX;
+    delete eixoY;
+
+    // Remove a list com as series
+    for (list<Serie*>::iterator it = series->begin(); it != series->end(); ++it)
+        delete *it;
+    series->clear();
+    delete series;
+
+    // Remove o vector com as series
+    //for (vector<Serie*>::iterator it = vectorSeries->begin(); it != vectorSeries->end(); ++it)
+    //    delete *it;
+    vectorSeries->clear();
+    delete vectorSeries;
+
+    // Remove instancia de InterfaceSerial
+    delete is;
 
     return 0;
 }
